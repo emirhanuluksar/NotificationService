33e1828cb125e07db3ee625c47c2606af11b836d
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-12-13 01:16:19 +0300

    Update

--- src/UseNotify/Entities/Notification.cs
+++ src/UseNotify/Entities/Notification.cs
@@ -1,7 +1,8 @@
+using UseNotify.Repositories;
+
 namespace UseNotify.Entities;
 
-public class Notification {
-    public int NotificationId { get; set; }
+public class Notification : Entity<int> {
     public int RecepeintId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
